e56e75fb621a90ce42719974ff4efbc1dcb3c4a3
JJ <macbook@MacBook-di-Jacopo-Tupac-Santaiti.local>
2013-07-06 18:33:53 +0200

    primo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calcolatrice::Application.config.secret_token = '29df4f214614af4adbecd12d671f61cd3b52d8e1b06cfc6eb74ad43e0168bb475ce578a96543b6ad5d73733983b03a75d71756bc60bd3f4e7ae059463e108bb3'
